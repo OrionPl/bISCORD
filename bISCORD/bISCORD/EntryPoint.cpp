0eddfcfa0f7b5b032f78ce0c544c34e5e996d509
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2019-08-19 14:16:30 +0200

    quik fix #1

--- bISCORD/bISCORD/EntryPoint.cpp
+++ bISCORD/bISCORD/EntryPoint.cpp
@@ -1,4 +1,5 @@
 #include <iostream> 
+#include <future>
 #include <WS2tcpip.h>
 #include <string>
 
@@ -6,7 +7,69 @@
 
 #pragma comment (lib, "ws2_32.lib")
 
-int main()
+SOCKET clientSock;
+std::string nick;
+
+void Receive()
+{
+	Helper help;
+
+	char buffer[4096];
+
+	while (true)
+	{
+		ZeroMemory(buffer, 4096);
+
+		int bytesReceived = recv(clientSock, buffer, 4096, 0);
+		std::string received = std::string(buffer, 0, bytesReceived);
+
+		if (bytesReceived > 0)
+		{
+			if (help.StringStartsWith(received, "msgfrom"))
+			{
+				received = help.ShortenStringFromLeft(received, 8);
+				std::string name = help.GetStringUntil(received, "###");
+				std::string msg = help.ShortenStringFromLeft(received, name.length() + 3);
+
+				if (name != nick)
+					std::cout << name << "> " << msg << std::endl;
+			}
+			else
+			{
+				std::cout << "SERVER> " << received << std::endl;
+			}
+		}
+		else
+		{
+			std::cerr << "SHIT! Server disconnected!" << std::endl;
+		}
+	}
+}
+
+void Send()
+{
+	std::string userInput;
+
+	do
+	{
+		std::cout << "> ";
+		std::cin >> userInput;
+
+		if (userInput.size() > 0)
+		{
+			userInput = "msg " + userInput;
+
+			int sendResult = send(clientSock, userInput.c_str(), userInput.size() + 1, 0);
+
+			if (sendResult == SOCKET_ERROR)
+			{
+				std::cout << "SHIT! Something went wrong when sending shit" << std::endl;
+			}
+		}
+	} while (userInput.size() > 0);
+}
+
+void InitializeClient()
 {
 	std::string serverIp = "127.0.0.1"; //external 89.78.12.9
 	int port = 8008;
@@ -23,7 +86,7 @@ int main()
 
 	//SOCKET CREATION
 
-	SOCKET clientSock = socket(AF_INET, SOCK_STREAM, 0);
+	clientSock = socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSock == INVALID_SOCKET)
 	{
 		std::cerr << "SHIT! Invalid socket" << std::endl;
@@ -46,67 +109,25 @@ int main()
 		std::cerr << "SHIT! Can't connect to server" << std::endl;
 		closesocket(clientSock);
 		WSACleanup();
-		return 0;
 	}
 
 	std::cout << "Connected to server" << std::endl << std::endl;
 
-	char buffer[4096];
-	std::string userInput;
-
 	std::cout << "Write your nickname" << std::endl;
 
-	std::string nick;
 	std::cin >> nick;
 
 	std::string userInfo = "userInfo " + nick;
 	send(clientSock, userInfo.c_str(), userInfo.size() + 1, 0);
 
 	std::cout << "Set nickname to " << nick << std::endl << std::endl;
+}
 
-	Helper help;
-
-	do 
-	{
-		std::cout << "> ";
-		std::cin >> userInput;
-
-		if (userInput.size() > 0)
-		{
-			userInput = "msg " + userInput;
-
-			int sendResult = send(clientSock, userInput.c_str(), userInput.size() + 1, 0);
-
-			if (sendResult != SOCKET_ERROR)
-			{
-				ZeroMemory(buffer, 4096);
-				
-				int bytesReceived = recv(clientSock, buffer, 4096, 0);
-				std::string received = std::string(buffer, 0, bytesReceived);
-
-				if (bytesReceived > 0)
-				{
-					if (help.StringStartsWith(received, "msgfrom"))
-					{
-						received = help.ShortenStringFromLeft(received, 8);
-						std::string name = help.GetStringUntil(received, "###");
-						std::string msg = help.ShortenStringFromLeft(received, name.length() + 3);
-
-						if (name != nick)
-						std::cout << name << "> " << msg << std::endl;
-					}
-					else
-					{
-						std::cout << "SERVER> " << received << std::endl;
-					}
-				}
-				else
-				{
-					std::cerr << "SHIT! Server disconnected!" << std::endl;
-				}
-			}
-		}
-	} while (userInput.size() > 0);
+int main()
+{
+	InitializeClient();
+	auto receiveAsync = std::async(std::launch::async, Receive);
+	Send();
 
 	closesocket(clientSock);
 	WSACleanup();
